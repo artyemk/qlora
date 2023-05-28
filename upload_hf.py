c0dc75de9114f557e8389c096553fbee75d6cc43
_kernelpanic <kernel@pan.ic>
2023-05-28 12:44:27 +0300

    fix

--- upload_hf.py
+++ upload_hf.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 api.upload_folder(
-    folder_path="/qlora_guanaco/",
+    folder_path="/qlora/output/checkpoint-final/adapter_model",
     repo_id="Kernel/qlora_guanaco",
     repo_type="model",
 )
